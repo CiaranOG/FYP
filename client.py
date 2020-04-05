70e323fb0c7414d72cbf43746c43c19e025ad6e1
Ciaran OG <ciaranogormann@gmail.com>
2020-04-05 23:42:04 +0100

    added functionality for private rooms

--- client.py
+++ client.py
@@ -1,20 +1,35 @@
-import socketio
+from socketIO_client import SocketIO, LoggingNamespace
 import time
 
-def on_aaa_response(args):
-    print('on_aaa_response', args['data'])
 
-socketIO = socketio.Client()
-socketIO.connect('http://localhost:5000')
-filepath = 'uploads/input.csv'
-with open(filepath) as fp:
-   line = fp.readline()
-   cnt = 1
-   while line:
+def room_created(args):
+    print('room_created', args)
+
+
+
+def my_response(args):
+    print("myresponse:",args)
+
+def page_joined(args):
+    filepath = 'uploads/input.csv'
+    with open(filepath) as fp:
        line = fp.readline()
-       socketIO.emit('my event',{'user_name': 'client','message' : line})
-       cnt += 1
-       time.sleep(.5)
-socketIO.emit('end transfer')
-time.sleep(2)
-socketIO.disconnect()
+       cnt = 1
+       while line:
+           line = fp.readline()
+           if len(line) == 0:
+               print('empty line')
+               break
+           socketIO.emit('my event',{'room': args,'message' : line})
+           cnt += 1
+           time.sleep(.5)
+    socketIO.emit('end transfer')
+    time.sleep(2)
+    socketIO.disconnect()
+
+socketIO = SocketIO('localhost', 5000, LoggingNamespace)
+socketIO.on('room_created', room_created)
+socketIO.on('page_joined', page_joined)
+socketIO.on('my response', my_response)
+socketIO.emit('create_room')
+socketIO.wait(seconds=20)
--- main.py
+++ main.py
@@ -6,8 +6,10 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, join_room, leave_room
 import json
+from flask_session import Session
+import random
 
 UPLOAD_FOLDER = 'uploads'
 DOWNLOAD_FOLDER = 'downloadables'
@@ -17,22 +19,13 @@ MODEL = pickle.load(open('models/label_spread.sav', 'rb'))
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
 app.config['SECRET_KEY'] = 'secret_key'
-socketio = SocketIO(app)
-
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+Session(app)
+socketio = SocketIO(app) #, manage_session=False
+ROOMS = []
 @app.route("/")
 def home():
     return render_template("home.html")
-@app.route("/run_model_dragdrop")
-def run_model_dragdrop():
-    return render_template("run_model_drag_drop.html")
-
-@app.route("/predict",methods = ['POST', 'GET'])
-def predict():
-    if request.method == 'POST':
-        result = request.form['nm']
-        return render_template("predict.html",result = result)
-
-
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -45,6 +38,7 @@ def uploaded_file(filename):
 
 @app.route('/run_model', methods=['GET', 'POST'])
 def run_model():
+
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
@@ -73,8 +67,28 @@ def run_model():
     return render_template("run_model.html",dataframe= None, predictions= None)
 from flask import send_from_directory
 
+@app.route('/enter_room', methods=['GET', 'POST'])
+def enter_room():
+    global ROOMS
+    if request.method == 'POST':
+        if len(request.form.get('code')) == 0 :
+            return render_template("enter_room.html", error = "No code entered")
+        if int(request.form.get('code')) not in ROOMS:
+            return render_template("enter_room.html", error = 'No room exists with code "{}".'.format(request.form.get('code')))
+        if int(request.form.get('code')) in ROOMS:
+            roomURL = "room/" + str(request.form.get('code'))
+            print("roomURL:",roomURL)
+            return redirect(roomURL)
 
+    return render_template("enter_room.html")
 
+@app.route('/room/<room>')
+def show_room(room):
+    global ROOMS
+    if int(room) in ROOMS:
+        return render_template("room.html", room_code = room)
+    else:
+        return render_template("room.html",room_code = None )
 
 @app.route("/stream_data")
 def stream_data():
@@ -84,15 +98,50 @@ def stream_data():
 def end_transfer():
     socketio.emit('end transfer', broadcast=True)
 
+@socketio.on('Webpage_enter_room')
+def Webpage_enter_room(room_code):
+    join_room(room_code)
+    socketio.emit("page_joined",str(room_code), room=room_code)
+    return
+
+@socketio.on('create_room')
+def create_room():
+    print(request.sid)
+    global ROOMS
+    room_assigned= False
+    while not room_assigned:
+        code = random.randint(1000, 9999)
+        if code not in ROOMS:
+            join_room(code)
+            ROOMS += [code]
+            print("Here:",ROOMS)
+            socketio.emit('room_created', code,room=request.sid)
+            print('sending message "{}" to room "{}".'.format(code, request.sid))
+            room_assigned = True
+
+
+
+
 @socketio.on('my event')
 def handle_my_custom_event(json, methods=['GET', 'POST']):
     print('received my event: ' + str(json))
+    line = json['message']
+    line = line.replace(",,",", -1,")
+    line = line.replace(",0,",", -1,")
+    line = line.strip('\n')
+    print(line)
+    df = pd.DataFrame(eval(line))
+    print('here')
+    prediction =  MODEL.predict(df.transpose())
+    json['message'] = line + ", " + str(prediction[0])
+    print('prediction:',prediction)
     f = open("downloadables/report.csv", "a")
-    f.write(json['message'])
+    f.write(line + ", " + str(prediction[0]) + "\n")
     f.close()
-    # df = pd.DataFrame(json['message'])
-    # print(df)
-    socketio.emit('my response', json, broadcast=True)
+    # df = pd.DataFrame(eval(json['message'].replace(",,","0")))
+    # print("",df)
+    print("json['room']:",json['room'])
+    socketio.emit('my response', json, room = int(json['room']) )
 
 
 if __name__ == "__main__":
